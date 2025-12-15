41d8ab6fe301f54a056f9b05825114c5fb86942b
oravid85 <oravid85@gmail.com>
2025-12-15 13:01:48 +0200

    lesson 28 end

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -6,8 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allownewServer = false
 
-  constructor() { }
+  constructor() { 
+    setTimeout(() => {
+     this.allownewServer = true; 
+    } , 2000);
+  }
 
   ngOnInit() {
   }
