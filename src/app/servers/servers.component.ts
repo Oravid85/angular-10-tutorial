8548b81c1b3d5b2f221c1f51ea27a1c48b8865af
oravid85 <oravid85@gmail.com>
2025-12-15 13:47:14 +0200

    lesson 34 end

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allownewServer = false
   servreCreationStatus = 'No server was created!'
-  serverName=""
+  serverName="Testserver"
 
   constructor() { 
     setTimeout(() => {
